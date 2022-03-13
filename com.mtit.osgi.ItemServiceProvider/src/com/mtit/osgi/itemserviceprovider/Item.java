30b136f079f181a247d55ebc2bf72378310579cd
Chanuth10 <it20074418@my.sliit.lk>
2022-03-13 09:43:19 +0530

    Adding Item class and create getter and setter methods

--- com.mtit.osgi.ItemServiceProvider/src/com/mtit/osgi/itemserviceprovider/Item.java
+++ com.mtit.osgi.ItemServiceProvider/src/com/mtit/osgi/itemserviceprovider/Item.java
@@ -4,49 +4,81 @@ public class Item {
      private String bName;
      private String bDesc; 
 	 private double bQty;
+	 private double bCategory;
      private double bPrice;
      
-     public Item(String bName, String bDesc, double bQty, double bPrice) {
- 		super();
- 		this.bName = bName;
- 		this.bDesc = bDesc;
- 		this.bQty = bQty;
- 		this.bPrice = bPrice;
- 	 }
+    
  
+	public Item(String bName, String bDesc, double bQty, double bCategory, double bPrice) {
+		super();
+		this.bName = bName;
+		this.bDesc = bDesc;
+		this.bQty = bQty;
+		this.bCategory = bCategory;
+		this.bPrice = bPrice;
+	}
+
+
+
 	public String getbName() {
 		return bName;
 	}
 
-	public String getbDesc() {
-		return bDesc;
-	}
 
-	public void setbDesc(String bDesc) {
-		this.bDesc = bDesc;
-	}
 
 	public void setbName(String bName) {
 		this.bName = bName;
 	}
 
 
+
+	public String getbDesc() {
+		return bDesc;
+	}
+
+
+
+	public void setbDesc(String bDesc) {
+		this.bDesc = bDesc;
+	}
+
+
+
 	public double getbQty() {
 		return bQty;
 	}
+
+
 
 	public void setbQty(double bQty) {
 		this.bQty = bQty;
 	}
 
+
+
+	public double getbCategory() {
+		return bCategory;
+	}
+
+
+
+	public void setbCategory(double bCategory) {
+		this.bCategory = bCategory;
+	}
+
+
+
 	public double getbPrice() {
 		return bPrice;
 	}
 
+
+
 	public void setbPrice(double bPrice) {
 		this.bPrice = bPrice;
 	}
-     
+
+	
      
      
 }
