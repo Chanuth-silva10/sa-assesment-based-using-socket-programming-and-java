d29900a22d366dde1695b11b942eb9db32b9e1f6
Chanuth10 <it20074418@my.sliit.lk>
2022-03-19 20:03:56 +0530

    EmployeeServiceProvider Activator class implement

--- EmployeeServiceProvider/src/com/mtit/osgi/employeeserviceprovider/Activator.java
+++ EmployeeServiceProvider/src/com/mtit/osgi/employeeserviceprovider/Activator.java
@@ -2,21 +2,23 @@ package com.mtit.osgi.employeeserviceprovider;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
+	ServiceRegistration publishServiceRegistration;
 
-	static BundleContext getContext() {
-		return context;
+	public void start(BundleContext Context) throws Exception {
+		
+		System.out.println("Publisher Employee Management Starter");
+		EmployeeService publishEmployeeService = new EmployeeServiceImpl();
+		publishServiceRegistration = Context.registerService(EmployeeService.class.getName(),publishEmployeeService , null);
+		
 	}
-
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
-	}
-
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+	public void stop(BundleContext Context) throws Exception {
+		System.out.println("Publisher Employee Management stop.");
+		publishServiceRegistration.unregister();
 	}
 
 }
